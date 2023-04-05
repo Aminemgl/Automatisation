6440d03201e344a844df8ab935aef3607eedd98d
AMINEDOWN <amineetd@gmail.com>
2023-04-05 15:31:25 +0200

    update file

---
+++ src/main/java/org/example/HomePage.java
@@ -0,0 +1,26 @@
+package org.example;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+
+public class HomePage {
+
+
+    By searchBarLocator = By.cssSelector("#twotabsearchtextbox");
+
+    By cookieAcceptButtonLocator = By.cssSelector("#sp-cc-accept");
+    WebDriver driver;
+
+    public HomePage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void search(String keyword) {
+        driver.findElement(searchBarLocator).sendKeys(keyword + Keys.ENTER);
+    }
+
+    public void closeCookiePopup() {
+        driver.findElement(cookieAcceptButtonLocator).click();
+    }
+}
